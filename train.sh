c4a3d2471931890f96d3839084d2933f3242163a
ChengDian9473 <160752520+ChengDian9473@users.noreply.github.com>
2026-01-29 19:05:19 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -3,8 +3,9 @@ cuda="0" # Select GPU
 
 CUDA_VISIBLE_DEVICES=$cuda XLA_PYTHON_CLIENT_PREALLOCATE=false PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
 python simpler_env/train_ms3_ppo.py \
-  --name="bottle_shovel-320-rand_scene-seed_0" \
-  --log="/workspace/Autonomous_RL/bottle_shovel-320-rand_scene-seed_0.txt" \
+  --name="cronos-bottle_shovel-320-rand_scene-seed_0" \
+  --log="cronos-bottle_shovel-320-rand_scene-seed_0.txt" \
+  --wandb_dir=".." \
   --env_id="TwoObjectTwoReceptacle-v1" \
   --vla_path="openvla/openvla-7b" --vla_unnorm_key="bridge_orig" \
   --seed=0
